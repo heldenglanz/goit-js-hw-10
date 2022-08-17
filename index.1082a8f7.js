9466e1b6b9b6275572b17b1e0b2e0db1f66d1c73
heldenglanz <73900017+heldenglanz@users.noreply.github.com>
2022-08-17 20:24:42 +0000

    Deploying to gh-pages from @ heldenglanz/goit-js-hw-10@5a0cf0caaf92038f33a45ed9d088eabaa01142db 🚀

--- index.1082a8f7.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/peru").then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}));document.querySelector('[id="search-box"]');
-//# sourceMappingURL=index.1082a8f7.js.map
--- index.3b5957af.js
+++
@@ -1,2 +0,0 @@
-!function(){fetch("https://restcountries.com/v3.1/name/peru").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));document.querySelector('[id="search-box"]')}();
-//# sourceMappingURL=index.3b5957af.js.map
