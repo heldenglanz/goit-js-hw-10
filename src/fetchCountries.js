5a0cf0caaf92038f33a45ed9d088eabaa01142db
Heldenglanz <heldenglanz@gmail.com>
2022-08-17 23:24:15 +0300

    soo

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,21 +1,75 @@
+// import countryCard from '//'
 export  {fetchCountries} 
-
+const countryDivEl = document.querySelector('.country-info');
 // const inputEl = document.querySelector('[id="search-box"]')
 
 // inputEl.addEventListener('input', fetchCountries);
-
+// fetchCountries(peru)
 // function fetchCountries(name){
+fetch('https://restcountries.com/v3.1/lang/{lang}')
+.then(response=>{
+  return response.json();
+})
+.then(data=>{
+  console.log(data)
+})
 
- fetch(`https://restcountries.com/v3.1/name/peru`)
+
+
+fetch(`https://restcountries.com/v3.1/all`)
     .then(response => {
         return response.json();
     })
-    .then(languages =>{
-      console.log(languages)
+    .then(data =>{
+      console.log(data);
+      // console.log()
+      return data;
     })
-    .catch (error =>{
-      console.log(error);
-    })
+    .then(renderCountry)
+    
+     
+    // }
+   
+    // .catch(error=> console.log(error))
+
+
+    // .then(country =>{
+    //   console.log(country);
+    //   const markup = countryCard(country)
+    //   console.log(markup)
+    // })
+
+
+    function renderCountry(data){
+      console.log(data)
+      
+      // countryDivEl.textContent = arr;
+  const markup = data.reduce((acc, item)=>{
+   
+   return (acc +
+    //  `<p>${item.languages}</p>`
+   `<div class="card">
+   <div class="card-img">
+       <img src="${item.flags.svg}" alt="${item.name.common.official}">
+   </div>
+   <div>
+       <h2 class="card-title">${item.name.official}</h2>
+       <p class="card-capital"><span>Capital:</span> ${item.capital}</p>
+       <p class="card-population"><span>Population:</span> ${item.population}</p>
+       <ul class="card-languages"><span>Languages:</span>
+           <li class="language-item">${item.languages}</li>
+       </ul>
+   </div>
+   
+   </div>`
+        );
+  
+  },
+  '');
+  
+  countryDivEl.innerHTML = markup;
+}
+
     // }
 
 // fetchCountries(peru)
