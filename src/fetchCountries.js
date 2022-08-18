961f68695bb7688cec3a40df0b5266144721f494
Heldenglanz <heldenglanz@gmail.com>
2022-08-18 14:34:54 +0300

    mn

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,74 +1,75 @@
 // import countryCard from '//'
 export  {fetchCountries} 
+// const countryDivEl = document.querySelector('.country-info');
+const inputEl = document.querySelector('[id="search-box"]')
 const countryDivEl = document.querySelector('.country-info');
+const countryListEl = document.querySelector('.country-list')
+
+inputEl.addEventListener('input', onInputChange);
+
+
+function onInputChange(e){
+    // e.preventDefault()
+
+const form = e.currentTarget;
+const inputResult = form.value.trim()
+console.log(inputResult)
+// const searchCountry = form.textContent;
+
+
+fetchCountries(inputResult)
+.then(renderCountry)
+.catch(error=> console.log(error))
+
+// if(!inputResult){
+//     countryDivEl.innerHTML = '';
+//     countryListEl.innerHTML = '';
+// Notiflix
+
+// }
+
+
+
+
+}
+
 // const inputEl = document.querySelector('[id="search-box"]')
 
 // inputEl.addEventListener('input', fetchCountries);
 // fetchCountries(peru)
-// function fetchCountries(name){
-fetch('https://restcountries.com/v3.1/lang/{lang}')
-.then(response=>{
-  return response.json();
-})
-.then(data=>{
-  console.log(data)
-})
 
+// fetch('https://restcountries.com/v3.1/lang/{lang}')
+// .then(response=>{
+//   return response.json();
+// })
+// .then(data=>{
+//   console.log(data)
+// })
+// fetchCountries()
+// .then(renderCountry)
+// .catch(error=> console.log(error))
 
-
-fetch(`https://restcountries.com/v3.1/all`)
+function fetchCountries(country){
+return fetch(`https://restcountries.com/v3.1/all`)
     .then(response => {
         return response.json();
     })
-    .then(data =>{
-      console.log(data);
-      // console.log()
-      return data;
-    })
-    .then(renderCountry)
+  }
+
+    // .then(data =>{
+    //   console.log(data);
+      
+    //   return data;
+    // })
     
-     
-    // }
-   
-    // .catch(error=> console.log(error))
 
-
+    
     // .then(country =>{
     //   console.log(country);
     //   const markup = countryCard(country)
     //   console.log(markup)
     // })
 
-
-    function renderCountry(data){
-      console.log(data)
-      
-      // countryDivEl.textContent = arr;
-  const markup = data.reduce((acc, item)=>{
-   
-   return (acc +
-    //  `<p>${item.languages}</p>`
-   `<div class="card">
-   <div class="card-img">
-       <img src="${item.flags.svg}" alt="${item.name.common.official}">
-   </div>
-   <div>
-       <h2 class="card-title">${item.name.official}</h2>
-       <p class="card-capital"><span>Capital:</span> ${item.capital}</p>
-       <p class="card-population"><span>Population:</span> ${item.population}</p>
-       <ul class="card-languages"><span>Languages:</span>
-           <li class="language-item">${item.languages}</li>
-       </ul>
-   </div>
-   
-   </div>`
-        );
-  
-  },
-  '');
-  
-  countryDivEl.innerHTML = markup;
-}
 
     // }
 
@@ -84,3 +85,29 @@ fetch(`https://restcountries.com/v3.1/all`)
 
 // }
 // inputEl.textContent
+function renderCountry(data){
+  console.log(data)
+  // countryDivEl.textContent = arr;
+const markup = data.reduce((acc, item)=>{
+
+return (acc +
+//  `<p>${item.languages}</p>`
+`<div class="card">
+<div class="card-img">
+   <img src="${item.flags.svg}" alt="${item.name.common.official}", width=180px>
+</div>
+<div>
+   <h2 class="card-title">${item.name.official}</h2>
+   <p class="card-capital"><span>Capital:</span> ${item.capital}</p>
+   <p class="card-population"><span>Population:</span> ${item.population}</p>
+   <ul class="card-languages"><span>Languages:</span>
+       <li class="language-item">${item.languages}</li>
+   </ul>
+</div>
+
+</div>`
+    );
+},
+'');
+countryDivEl.innerHTML = markup;
+}
--- src/index.js
+++ src/index.js
@@ -2,20 +2,65 @@ import './css/styles.css';
 import fetchCountries from './fetchCountries'
 // const DEBOUNCE_DELAY = 300;
 
-
-
-
 const inputEl = document.querySelector('[id="search-box"]')
+const countryDivEl = document.querySelector('.country-info');
+const countryListEl = document.querySelector('.country-list')
 
-// inputEl.addEventListener('input', fetchCountries);
+inputEl.addEventListener('input', onInputChange);
 
-// function fetchCountries(name){
 
-//  fetch(`https://restcountries.com/v3.1/name/peru`)
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(country =>{
-//       console.log(country)
-//     })
-    // }
+function onInputChange(e){
+    // e.preventDefault()
+
+const form = e.currentTarget;
+const inputResult = form.value.trim()
+console.log(inputResult)
+// const searchCountry = form.textContent;
+
+
+fetchCountries(inputResult)
+.then(renderCountry)
+.catch(error=> console.log(error))
+
+// if(!inputResult){
+//     countryDivEl.innerHTML = '';
+//     countryListEl.innerHTML = '';
+// Notiflix
+
+// }
+
+
+
+
+}
+
+
+
+
+
+function renderCountry(data){
+    console.log(data)
+    // countryDivEl.textContent = arr;
+const markup = data.reduce((acc, item)=>{
+ 
+ return (acc +
+  //  `<p>${item.languages}</p>`
+ `<div class="card">
+ <div class="card-img">
+     <img src="${item.flags.svg}" alt="${item.name.common.official}", width=180px>
+ </div>
+ <div>
+     <h2 class="card-title">${item.name.official}</h2>
+     <p class="card-capital"><span>Capital:</span> ${item.capital}</p>
+     <p class="card-population"><span>Population:</span> ${item.population}</p>
+     <ul class="card-languages"><span>Languages:</span>
+         <li class="language-item">${item.languages}</li>
+     </ul>
+ </div>
+ 
+ </div>`
+      );
+},
+'');
+countryDivEl.innerHTML = markup;
+}
