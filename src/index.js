70629e227a6f79ca1f12ec1445093ba08fabb263
Heldenglanz <heldenglanz@gmail.com>
2022-08-19 12:03:25 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/styles.css';
-const DEBOUNCE_DELAY = 300;
 import Notiflix from 'notiflix';
 import API from './fetchCountries'
+import countrycard from './templates/countrycard.hbs'
 const debounce = require('lodash.debounce');
-
+const DEBOUNCE_DELAY = 300;
 
 
 const inputEl = document.querySelector('[id="search-box"]')
@@ -13,7 +13,7 @@ const countryListEl = document.querySelector('.country-list')
 inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
 function onInputChange(e){
-    const inputResult = e.target.value;
+    const inputResult = e.target.value.trim();
 
 
 if(!inputResult){
@@ -28,25 +28,27 @@ API.fetchCountries(inputResult)
         Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
     }
     else if (response.length === 1){
-        renderCountry(response[0]);
-        renderCountryInfo()
+        
+        renderCountryInfo(response[0])
     }
     else if(response.length>1&&response.length<=10){
-        renderCountry(response);
-        renderCountryList()
+       
+        renderCountryList(response)
     }
 })
 .catch(onError)
+
 }
 function renderCountryInfo(search){
-    renderCountry(search)
+    const markup = countrycard(search)
     countryListEl.innerHTML = '';
     countryDivEl.innerHTML = markup;
 }
 function renderCountryList(arr){
-    const markup = arr.reduce((acc, {name, flag})=>{
-        return (acc+ `<li class="country-list-item">
-        <img class="country-list-flag" src="${flag.svg})" alt="${name.official}" width="80" heigth="60">
+    const markup = arr.reduce((acc, {name, flags})=>{
+        return (acc +
+            `<li class="country-list-item">
+        <img class="country-list-flag" src="${flags.svg}" alt="${name.official}" width="20" heigth="10">
         <p class="country-list-name">${name.official}</p>
         </li>`
         );
@@ -57,30 +59,32 @@ function renderCountryList(arr){
 function onError(error){
     countryDivEl.innerHTML = '';
     countryListEl.innerHTML = '';
+    Notiflix.Notify.failure('Oops, there is no country with that name');
 }
+
 // console.log(searchQuery)
 // const searchCountry = form.textContent;
 
-function renderCountry(data){
-    // console.log(data);
-    // countryDivEl.textContent = arr;
-  const markup = data.reduce((acc, country)=>{
-  return (acc +
-  `<div class="card">
-  <div class="card-img">
-     <img src="${country.flags.svg}" alt="${country.name.common.official}", width=180px>
-  </div>
-  <div>
-     <h2 class="card-title">${country.name.official}</h2>
-     <p class="card-capital"><span>Capital:</span> ${country.capital}</p>
-     <p class="card-population"><span>Population:</span> ${country.population}</p>
-     <p class="card-languages"><span>Languages:</span>
-         <span class="language-item">${country.languages}</span>
-     </p>
-  </div>
-  </div>`
-      );
-  },
-  '');
-  countryDivEl.innerHTML = markup;
-  }
+// function renderCountry(search){
+//     // console.log(data);
+//     // countryDivEl.textContent = arr;
+//   const markup = search.reduce((acc, item)=>{
+//   return (acc +
+//   `<div class="card">
+//   <div class="card-img">
+//      <img src="${item.flags.svg}" alt="${item.name.common.official}", width=180px>
+//   </div>
+//   <div>
+//      <h2 class="card-title">${item.name.official}</h2>
+//      <p class="card-capital"><span>Capital:</span> ${item.capital}</p>
+//      <p class="card-population"><span>Population:</span> ${item.population}</p>
+//      <p class="card-languages"><span>Languages:</span>
+//          <span class="language-item">${item.languages}</span>
+//      </p>
+//   </div>
+//   </div>`
+//       );
+//   },
+//   '');
+//   countryDivEl.innerHTML = markup;
+//   }
